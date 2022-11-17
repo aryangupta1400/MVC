146c0777337e41bde2182566324a8bf7f201968c
Aryan Gupta <Aryan.Gupta@oneadvanced.com>
2022-11-17 18:04:48 +0530

    Add project files.

---
+++ MVC/Models/UserModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Mobile")]
+        public int Mobile { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+    }
+}
